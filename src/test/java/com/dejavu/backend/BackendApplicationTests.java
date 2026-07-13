15bd695eaf6791823fff248ead07c70941181b2b
sebinJeong <tpqls8791@gmail.com>
2026-07-13 10:27:51 +0900

    [Fix]_13/BackendApplicationTests-WIP
    - develop 병합 후 auth repository mock 누락 수정
    
    - contextLoads 테스트 통과 확인
    
    - 작업중 임시 커밋

--- src/test/java/com/dejavu/backend/BackendApplicationTests.java
+++ src/test/java/com/dejavu/backend/BackendApplicationTests.java
@@ -2,9 +2,11 @@ package com.dejavu.backend;
 
 import static org.mockito.Mockito.mock;
 
+import com.dejavu.backend.auth.repository.UserRefreshTokenRepository;
 import com.dejavu.backend.jobnotice.repository.JobNoticeRepository;
 import com.dejavu.backend.resume.repository.ResumeProjectRepository;
 import com.dejavu.backend.resume.repository.ResumeRepository;
+import com.dejavu.backend.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -43,6 +45,16 @@ class BackendApplicationTests {
 		@Bean
 		JobNoticeRepository jobNoticeRepository() {
 			return mock(JobNoticeRepository.class);
+		}
+
+		@Bean
+		UserRepository userRepository() {
+			return mock(UserRepository.class);
+		}
+
+		@Bean
+		UserRefreshTokenRepository userRefreshTokenRepository() {
+			return mock(UserRefreshTokenRepository.class);
 		}
 	}
 }
